40dddb893e62fd5b92d030eb788350bbcef6d268
Badrú <sbsbadroo@gmail.com>
2024-07-29 16:59:46 +0200

    Adding back-end

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const userRoutes = require('./routes/users.routes')
+const connect = require('../DB/database');
+const app = express();
+
+const port = process.env.PORT || 8000;
+app.use(express.json());
+connect();
+
+app.use('/api/',userRoutes)
+app.listen(port,()=>console.log(`Servidor rodando na porta localhost:${port}`))
